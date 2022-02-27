57e083d54b161809df6e702cd3a6c025e2e32b11
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-02-27 17:31:30 +0200

    Scaffolding

---
+++ MyTestedAspNetCoreApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyTestedAspNetCoreApp.Data;
+using MyTestedAspNetCoreApp.Models;
+
+[assembly: HostingStartup(typeof(MyTestedAspNetCoreApp.Areas.Identity.IdentityHostingStartup))]
+namespace MyTestedAspNetCoreApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
