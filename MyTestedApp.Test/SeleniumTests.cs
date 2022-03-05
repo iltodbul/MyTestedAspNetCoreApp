991c18f7c6624bd1c6a6c8144b186cc4d95e1599
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-03-05 16:40:29 +0200

    Selenium test

---
+++ MyTestedApp.Test/SeleniumTests.cs
@@ -0,0 +1,28 @@
+﻿using MyTestedAspNetCoreApp;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using Xunit;
+
+namespace MyTestedApp.Test
+{
+    public class SeleniumTests
+    {
+        [Fact]
+        public void H1ElementIsRemovedFromPrivacyPage()
+        {
+            var serverFactory = new SeleniumServerFactory<Startup>();
+
+            var options = new ChromeOptions();
+            options.AddArguments("--headless");
+            options.AcceptInsecureCertificates = true;
+
+            var webDriver = new ChromeDriver(options);
+
+            webDriver.Navigate().GoToUrl(serverFactory.RootUri + "/Home/Privacy");
+
+            Assert.Throws<NoSuchElementException>(() => webDriver.FindElement(By.TagName("p")));
+        }
+    }
+}
