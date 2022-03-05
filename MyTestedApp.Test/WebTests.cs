655f48d51ef7d788e108782b005fdcdbd69b903d
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2022-03-05 15:47:23 +0200

    Integration test with Microsoft.AspNetCore.Mvc.Testing

---
+++ MyTestedApp.Test/WebTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using MyTestedAspNetCoreApp;
+
+using Xunit;
+
+namespace MyTestedApp.Test
+{
+    public class WebTests
+    {
+        [Fact]
+        public async Task HomePageShouldHaveWelcomeHeading()
+        {
+            var webApplicationFactory = new WebApplicationFactory<Startup>();
+
+            HttpClient client = webApplicationFactory.CreateClient();
+
+            var response = await client.GetAsync("/");
+
+            response.EnsureSuccessStatusCode();
+
+            var html = await response.Content.ReadAsStringAsync();
+
+            Assert.Contains("<h2>Welcome in my app!!!</h2>", html);
+
+            Assert.True(response.Headers.Contains("x-info-action-name"));
+        }
+    }
+}
